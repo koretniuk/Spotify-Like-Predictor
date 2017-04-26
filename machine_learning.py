f1b94b657def0c09571969516719f965c65e7e0e
koretniuk <yurko.koretniuk@gmail.com>
2017-04-25 23:07:23 -0700

    Update machine_learning.py

--- machine_learning.py
+++ machine_learning.py
@@ -1,46 +1,20 @@
-
-# coding: utf-8
-
 # For this project I will be using three modules:
 # * GaussianNB from sklearn.naive_bayes which will be the learner and predictor
 # * pandas for manipulation with csv files
 # * dateutil.parser to clean up the data
 
-# In[2]:
-
 from sklearn.naive_bayes import GaussianNB
 import pandas as pd
 import dateutil.parser as parser
-
-
-# In[3]:
 
 # Creating classifyer
 
 clf = GaussianNB()
 
-
-# In[4]:
-
-# train is used interchangebly with fit. So when we say fit it means - train. 
-# clf.fit(Var1, Var2), where Var1 - features and Var2 - labels
-#clf.predict() - prediction based on trained data. We put Var1 (feature) on input and receive Var (label) on output
-# var pred - vector of predictions
-# accuracy is no. of points classified correctly / all points (in test set)
-# to determine accuracy, clf.score(X, y, sample_weight=None) is used. 
-# another method is to use accurac_score from sklearn.metrics import accuracy_score
-# accuracy_score(pred, label_test)
-
-
-# In[5]:
-
 # Loading features (here - pl) and labels (here - labels) using pandas
 
 pl = pd.read_csv('songs_i_like.csv')
 labels = pd.read_csv('labels.csv')
-
-
-# In[6]:
 
 #creating DataFrames based on imported csv-files
 
@@ -54,9 +28,6 @@ labels = pd.DataFrame(labels)
 
 pl = pl.drop(pl.columns[[0, 1, 2]], axis=1)
 
-
-# In[7]:
-
 # Creating array out of DataFrames with features to keep them as sub-arrays [[1,2,3],[1,2,3]]
 
 X = pl.as_matrix()
@@ -64,9 +35,6 @@ X = pl.as_matrix()
 # labels need to be one dimensional array, so I am converting it to that [[1],[0]] -> [1,0]
 
 labels = labels.values.flatten()
-
-
-# In[8]:
 
 # At this point of time I decided that the full date format is not necessary, so I am extracting year only.
 # At the same time I am extracting minutes value (without seconds) to simplify it a bit and convert to int.
@@ -77,9 +45,6 @@ while i < len(X):
     X[i][6] = parser.parse(X[i][6]).hour
     i += 1
 
-
-# In[9]:
-
 # Launching learning with .fit()
 
 clf.fit(X, labels)
@@ -88,16 +53,9 @@ clf.fit(X, labels)
 
 clf.score(X, labels)
 
-
-# In[10]:
-
 # Entering song by t.a.t.u - not gonna get us (my childhood's crush) to verify accuracy. 
 # Expecting 1 (like) or 0 (dislike).
 
 clf.predict([2014,120,89,72,-6,80,5,0,14])
 
-
-# In[ ]:
-
-
-
+# looks like it is working!
